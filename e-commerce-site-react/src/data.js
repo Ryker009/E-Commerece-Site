8abb2b2c45fc71ff7c38dd6739bb059fcd524c9f
Rakesh Kumar <rakesh9803646587@gmail.com>
2025-07-16 15:54:54 +0530

    file added

--- e-commerce-site-react/src/data.js
+++ e-commerce-site-react/src/data.js
@@ -43,45 +43,129 @@ const products = [
   },
   {
     id: 7,
+    name: "Mouse",
+    price: 799,
+    image: "/img/mouse.jpg",
+    category: "Electronics"
+  },
+  {
+    id: 8,
+    name: "Denim Jacket",
+    price: 2999,
+    image: "/img/denim-jacket.jpg",
+    category: "Fashion"
+  },
+  {
+    id: 9,
+    name: "Sunglasses",
+    price: 99,
+    image: "/img/Sunglasses.jpg",
+    category: "Accessories"
+  },
+  {
+    id: 10,
+    name: "Bag",
+    price: 1099,
+    image: "/img/laptop_bag.jpg",
+    category: "Bags"
+  },
+  {
+    id: 11,
+    name: "Casual Shoes",
+    price: 1299,
+    image: "/img/sneaker.jpg",
+    category: "Fashion"
+  },
+  {
+    id: 12,
+    name: "Wallet",
+    price: 599,
+    image: "/img/wallet.webp",
+    category: "Accessories"
+  },
+  {
+    id: 13,
     name: "Wireless Headphones",
     price: 1999,
     image: "/img/headphone.avif",
     category: "Electronics"
   },
   {
-    id: 8,
+    id: 14,
     name: "Smart Watch",
     price: 2499,
     image: "/img/watch.jpg",
     category: "Accessories"
   },
   {
-    id: 9,
+    id: 15,
     name: "Bluetooth Speaker",
     price: 1499,
     image: "/img/speaker.webp",
     category: "Electronics"
   },
   {
-    id: 10,
+    id: 16,
     name: "Casual Shoes",
     price: 999,
     image: "/img/sneaker.jpg",
     category: "Fashion"
   },
   {
-    id: 11,
+    id: 17,
     name: "Laptop Bag",
     price: 899,
     image: "/img/laptop_bag.jpg",
     category: "Bags"
   },
   {
-    id: 12,
+    id: 18,
     name: "Gaming Laptop",
     price: 49999,
     image: "/img/laptop.jpg",
     category: "Electronics"
+  },
+  {
+    id: 19,
+    name: "Mouse",
+    price: 799,
+    image: "/img/mouse.jpg",
+    category: "Electronics"
+  },
+  {
+    id: 20,
+    name: "Denim Jacket",
+    price: 2999,
+    image: "/img/denim-jacket.jpg",
+    category: "Fashion"
+  },
+  {
+    id: 21,
+    name: "Sunglasses",
+    price: 99,
+    image: "/img/Sunglasses.jpg",
+    category: "Accessories"
+  },
+  {
+    id: 22,
+    name: "Bag",
+    price: 1099,
+    image: "/img/laptop_bag.jpg",
+    category: "Bags"
+  },
+  {
+    id: 23,
+    name: "Casual Shoes",
+    price: 1299,
+    image: "/img/sneaker.jpg",
+    category: "Fashion"
+  },
+  {
+    id: 24,
+    name: "Wallet",
+    price: 599,
+    image: "/img/wallet.webp",
+    category: "Accessories"
   }
 ];
 
