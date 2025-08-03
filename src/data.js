00102c050513f96216957f1c4803bb71fb069b15
Rakesh Kumar <rakesh9803646587@gmail.com>
2025-08-04 01:14:17 +0530

    image address change

--- src/data.js
+++ src/data.js
@@ -3,168 +3,168 @@ const products = [
     id: 1,
     name: "Wireless Headphones",
     price: 1999,
-    image: "/img/headphone.avif",
+    image: `${import.meta.env.BASE_URL}img/headphone.avif`,
     category: "Electronics"
   },
   {
     id: 2,
     name: "Smart Watch",
     price: 2499,
-    image: "/img/watch.jpg",
+    image: `${import.meta.env.BASE_URL}img/watch.jpg`,
     category: "Accessories"
   },
   {
     id: 3,
     name: "Bluetooth Speaker",
     price: 1499,
-    image: "/img/speaker.webp",
+    image: `${import.meta.env.BASE_URL}img/speaker.webp`,
     category: "Electronics"
   },
   {
     id: 4,
     name: "Casual Shoes",
     price: 999,
-    image: "/img/sneaker.jpg",
+    image: `${import.meta.env.BASE_URL}img/sneaker.jpg`,
     category: "Fashion"
   },
   {
     id: 5,
     name: "Laptop Bag",
     price: 899,
-    image: "/img/laptop_bag.jpg",
+    image: `${import.meta.env.BASE_URL}img/laptop_bag.jpg`,
     category: "Bags"
   },
   {
     id: 6,
     name: "Gaming Laptop",
     price: 49999,
-    image: "/img/laptop.jpg",
+    image: `${import.meta.env.BASE_URL}img/laptop.jpg`,
     category: "Electronics"
   },
   {
     id: 7,
     name: "Mouse",
     price: 799,
-    image: "/img/mouse.jpg",
+    image: `${import.meta.env.BASE_URL}img/mouse.jpg`,
     category: "Electronics"
   },
   {
     id: 8,
     name: "Denim Jacket",
     price: 2999,
-    image: "/img/denim-jacket.jpg",
+    image: `${import.meta.env.BASE_URL}img/denim-jacket.jpg`,
     category: "Fashion"
   },
   {
     id: 9,
     name: "Sunglasses",
     price: 99,
-    image: "/img/Sunglasses.jpg",
+    image: `${import.meta.env.BASE_URL}img/Sunglasses.jpg`,
     category: "Accessories"
   },
   {
     id: 10,
     name: "Bag",
     price: 1099,
-    image: "/img/laptop_bag.jpg",
+    image: `${import.meta.env.BASE_URL}img/laptop_bag.jpg`,
     category: "Bags"
   },
   {
     id: 11,
     name: "Casual Shoes",
     price: 1299,
-    image: "/img/sneaker.jpg",
+    image: `${import.meta.env.BASE_URL}img/sneaker.jpg`,
     category: "Fashion"
   },
   {
     id: 12,
     name: "Wallet",
     price: 599,
-    image: "/img/wallet.webp",
+    image: `${import.meta.env.BASE_URL}img/wallet.webp`,
     category: "Accessories"
   },
   {
     id: 13,
     name: "Wireless Headphones",
     price: 1999,
-    image: "/img/headphone.avif",
+    image: `${import.meta.env.BASE_URL}img/headphone.avif`,
     category: "Electronics"
   },
   {
     id: 14,
     name: "Smart Watch",
     price: 2499,
-    image: "/img/watch.jpg",
+    image: `${import.meta.env.BASE_URL}img/watch.jpg`,
     category: "Accessories"
   },
   {
     id: 15,
     name: "Bluetooth Speaker",
     price: 1499,
-    image: "/img/speaker.webp",
+    image: `${import.meta.env.BASE_URL}img/speaker.webp`,
     category: "Electronics"
   },
   {
     id: 16,
     name: "Casual Shoes",
     price: 999,
-    image: "/img/sneaker.jpg",
+    image: `${import.meta.env.BASE_URL}img/sneaker.jpg`,
     category: "Fashion"
   },
   {
     id: 17,
     name: "Laptop Bag",
     price: 899,
-    image: "/img/laptop_bag.jpg",
+    image: `${import.meta.env.BASE_URL}img/laptop_bag.jpg`,
     category: "Bags"
   },
   {
     id: 18,
     name: "Gaming Laptop",
     price: 49999,
-    image: "/img/laptop.jpg",
+    image: `${import.meta.env.BASE_URL}img/laptop.jpg`,
     category: "Electronics"
   },
   {
     id: 19,
     name: "Mouse",
     price: 799,
-    image: "/img/mouse.jpg",
+    image: `${import.meta.env.BASE_URL}img/mouse.jpg`,
     category: "Electronics"
   },
   {
     id: 20,
     name: "Denim Jacket",
     price: 2999,
-    image: "/img/denim-jacket.jpg",
+    image: `${import.meta.env.BASE_URL}img/denim-jacket.jpg`,
     category: "Fashion"
   },
   {
     id: 21,
     name: "Sunglasses",
     price: 99,
-    image: "/img/Sunglasses.jpg",
+    image: `${import.meta.env.BASE_URL}img/Sunglasses.jpg`,
     category: "Accessories"
   },
   {
     id: 22,
     name: "Bag",
     price: 1099,
-    image: "/img/laptop_bag.jpg",
+    image: `${import.meta.env.BASE_URL}img/laptop_bag.jpg`,
     category: "Bags"
   },
   {
     id: 23,
     name: "Casual Shoes",
     price: 1299,
-    image: "/img/sneaker.jpg",
+    image: `${import.meta.env.BASE_URL}img/sneaker.jpg`,
     category: "Fashion"
   },
   {
     id: 24,
     name: "Wallet",
     price: 599,
-    image: "/img/wallet.webp",
+    image: `${import.meta.env.BASE_URL}img/wallet.webp`,
     category: "Accessories"
   }
 ];
